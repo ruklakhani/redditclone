3afaaa58d389b8bfb1ea9d0cb61768bddd10189c
ruk <ruk.lakhani@students.makeschool.com>
2020-04-14 16:35:43 -0700

    Users can submit and save posts'

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+require('./controllers/posts.js')(app);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
