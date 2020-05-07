b876800d7467ae8d9f911575d7d00b590e73b64d
ruk <ruk.lakhani@students.makeschool.com>
2020-05-06 18:36:18 -0700

    Users can see individual posts

--- controllers/post.js
+++ controllers/post.js
@@ -13,8 +13,19 @@ module.exports = (app) => {
       return res.redirect(`/`);
     })
   });
+
   app.get('/posts/new', (req, res) => {
     res.render('posts-new');
   });
 
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 };
