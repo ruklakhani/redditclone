de04a2eed454a2c6eb6724ae3eebcfdff9117f3f
ruk <ruk.lakhani@students.makeschool.com>
2020-05-07 21:32:14 -0700

    Fixed posts test

--- test/posts.js
+++ test/posts.js
@@ -2,6 +2,7 @@
 const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const agent = chai.request.agent(app);
 const expect = chai.expect;
 
 // Import the Post model from our models folder so we
@@ -20,6 +21,22 @@ describe('Posts', function() {
       url: 'https://www.google.com',
       summary: 'post summary'
   };
+  const user = {
+      username: 'poststest',
+      password: 'testposts'
+  };
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
   it("should create with valid attributes at POST /posts/new", function (done) {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -53,6 +70,22 @@ describe('Posts', function() {
     });
     });
 });
-after(function () {
-    Post.findOneAndDelete(newPost);
+after(function (done) {
+    Post.findOneAndDelete(newPost)
+    .then(function (res) {
+        agent.close()
+  
+        User.findOneAndDelete({
+            username: user.username
+        })
+          .then(function (res) {
+              done()
+          })
+          .catch(function (err) {
+              done(err);
+          });
+    })
+    .catch(function (err) {
+        done(err);
+    });
   });
