79879cc3d3baed6e5b91a933d8cf752314da8fd2
ruk <ruk.lakhani@students.makeschool.com>
2020-05-07 21:25:25 -0700

    Authors for comments are visible

--- controllers/post.js
+++ controllers/post.js
@@ -32,18 +32,19 @@ module.exports = (app) => {
     res.render('posts-new');
   });
 
+  // SHOW
   app.get("/posts/:id", function (req, res) {
     var currentUser = req.user;
     // LOOK UP THE POST
 
-    Post.findById(req.params.id).populate('comments').populate('author')
+    Post.findById(req.params.id).populate({path:'comments', populate: {path: 'author'}}).populate('author').lean()
         .then(post => {
             res.render("posts-show", { post, currentUser });  
         })
         .catch(err => {
             console.log(err.message);
         });
-});
+  });
 
 
   // SUBREDDIT
