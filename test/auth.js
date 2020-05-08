db09911348982eb3703b944661b90e3053761eb1
ruk <ruk.lakhani@students.makeschool.com>
2020-05-07 21:33:05 -0700

    Implemented remaining authentication tests

--- test/auth.js
+++ test/auth.js
@@ -26,6 +26,27 @@ it("should be able to signup", function(done) {
     });
   });
 
+  // login
+it("should be able to login", function(done) {
+    agent
+      .post("/login")
+      .send({ username: "testone", password: "password" })
+      .end(function(err, res) {
+        res.should.have.status(200);
+        agent.should.have.cookie("nToken");
+        done();
+      });
+  });
+
+  // logout
+it("should be able to logout", function(done) {
+    agent.get("/logout").end(function(err, res) {
+      res.should.have.status(200);
+      agent.should.not.have.cookie("nToken");
+      done();
+    });
+  });
+
   it("should not be able to login if they have not registered", function(done) {
     agent.post("/login", { email: "wrong@wrong.com", password: "nope" }).end(function(err, res) {
       res.status.should.be.equal(401);
